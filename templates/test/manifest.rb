6a8576d57dd122a348e046f78cad32a2204ab393
Phize <info@phize.net>
2011-07-29 13:35:08 +0900

    add test cases for each grid system.

---
+++ templates/test/manifest.rb
@@ -0,0 +1,34 @@
+description 'The X Grid System Test Suite.'
+
+html 'index.html', :to => 'test/index.html', :erb => true
+html 'xgs.html', :to => 'test/xgs.html', :erb => true
+html '300gs.html', :to => 'test/300gs.html', :erb => true
+html '748gs.html', :to => 'test/748gs.html', :erb => true
+html '940gs-12.html', :to => 'test/940gs-12.html', :erb => true
+html '940gs-16.html', :to => 'test/940gs-16.html', :erb => true
+html '978gs.html', :to => 'test/978gs.html', :erb => true
+html '1218gs.html', :to => 'test/1218gs.html', :erb => true
+html '1378gs.html', :to => 'test/1378gs.html', :erb => true
+html 'fluid_gs.html', :to => 'test/fluid_gs.html', :erb => true
+html 'responsive_gs.html', :to => 'test/responsive_gs.html', :erb => true
+
+stylesheet 'scss/test.scss', :media => 'screen', :to => 'test/test.scss'
+stylesheet 'scss/xgs.scss', :media => 'screen', :to => 'test/xgs.scss'
+stylesheet 'scss/300gs.scss', :media => 'screen', :to => 'test/300gs.scss'
+stylesheet 'scss/748gs.scss', :media => 'screen', :to => 'test/748gs.scss'
+stylesheet 'scss/940gs-12.scss', :media => 'screen', :to => 'test/940gs-12.scss'
+stylesheet 'scss/940gs-16.scss', :media => 'screen', :to => 'test/940gs-16.scss'
+stylesheet 'scss/978gs.scss', :media => 'screen', :to => 'test/978gs.scss'
+stylesheet 'scss/1218gs.scss', :media => 'screen', :to => 'test/1218gs.scss'
+stylesheet 'scss/1378gs.scss', :media => 'screen', :to => 'test/1378gs.scss'
+stylesheet 'scss/fluid_gs.scss', :media => 'screen', :to => 'test/fluid_gs.scss'
+stylesheet 'scss/responsive_gs.scss', :media => 'screen', :to => 'test/responsive_gs.scss'
+
+image 'img/xgs-grid.png', :to => 'test/xgs-grid.png'
+image 'img/300gs-grid.png', :to => 'test/300gs-grid.png'
+image 'img/748gs-grid.png', :to => 'test/748gs-grid.png'
+image 'img/940gs-12-grid.png', :to => 'test/940gs-12-grid.png'
+image 'img/940gs-16-grid.png', :to => 'test/940gs-16-grid.png'
+image 'img/978gs-grid.png', :to => 'test/978gs-grid.png'
+image 'img/1218gs-grid.png', :to => 'test/1218gs-grid.png'
+image 'img/1378gs-grid.png', :to => 'test/1378gs-grid.png'
