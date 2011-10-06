947bc7ae0717288e5b9e81e638ee2199892f1571
Phize <info@phize.net>
2011-10-07 07:26:06 +0900

    improve the way to get extension's path.

--- lib/xgs.rb
+++ lib/xgs.rb
@@ -1,2 +1,3 @@
 require 'compass'
-Compass::Frameworks.register("xgs", :path => "#{File.dirname(__FILE__)}/..")
+extension_path = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+Compass::Frameworks.register('xgs', :path => extension_path)
